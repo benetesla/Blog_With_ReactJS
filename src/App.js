66cebcb2f0339b04008509dfb77c09b77cb3d492
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-06 17:46:13 -0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import CreatPost from './pages/CreatePost';
 import Login from './pages/Login';
 function App() {
   return <Router>
+    < nav style={styles.nav}>
+      <Link style={styles.link} to="/">Home</Link>
+      <Link style={styles.link} to="/create">Create Post</Link>
+      <Link style={styles.link} to="/login">Login</Link>
+    </nav>
     <Routes>  
       <Route path="/" element={<Home />} />
       <Route path="/create" element={<CreatPost />} />
@@ -13,4 +18,20 @@ function App() {
   </Router>
 }
 
+const styles = {
+  nav: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: '10vh',
+    width: '100%',
+    background: 'gray',
+    color: 'white',
+    boxShadow: '0 0 10px 0 rgba(0,0,0,0.5)',
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'white',
+  },
+};
 export default App;
