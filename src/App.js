1fd55d806f67b83756b7281bdb89bb05d69e3f8b
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-06 15:17:42 -0300

    add Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
 function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+  return 
+  <Router>
+    <Routes>  
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
 }
 
 export default App;
