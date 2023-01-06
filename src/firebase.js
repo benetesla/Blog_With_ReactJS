377779ce22e7445ac422811c4f5bf17dd7114c9c
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-06 14:43:01 -0300

    Firebase authentication,With google AuthProvider

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyAhfCCAGmeLTW1uQbWEdYWgZEhE3a8q2Pk",
+  authDomain: "blogdev-230df.firebaseapp.com",
+  projectId: "blogdev-230df",
+  storageBucket: "blogdev-230df.appspot.com",
+  messagingSenderId: "452365884126",
+  appId: "1:452365884126:web:8bc515c4952efd8c5b804f",
+  measurementId: "G-J22GP7636F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
