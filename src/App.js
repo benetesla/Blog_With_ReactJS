487e5e72995e6d7d5c163fa2e590e8387d1dddcf
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-06 20:42:10 -0300

    Finishing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
-import Home from './pages/Home';
-import CreatPost from './pages/CreatePost';
-import Login from './pages/Login';
-function App() {
-  return <Router>
-    <nav style={styles.nav}>
-      <Link style={styles.link} to="/">HOME</Link>
-      <Link style={styles.link} to="/create">NEW POST</Link>
-      <Link style={styles.link} to="/login">LOGIN</Link>
-    </nav>
-    <Routes>  
-      <Route path="/" element={<Home />} />
-      <Route path="/create" element={<CreatPost />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </Router>
-}
-const styles = {
-  nav: {
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    height: '10vh',
-    width: '100%',
-    background: 'linear-gradient(50deg, #FE6B8B 25%, #FF8E53 10%)',
-    color: 'white',
-    boxShadow: '0 0 10px 0 rgba(0,0,0,0.5)',
-    fontfamily: 'Roboto',
-    TextTransform: 'uppercase',
-  },
-  link: {
-    textDecoration: 'none',
-    color: 'white',
-    TextTransform: 'uppercase',
-    fontWeight: 'bold',
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
-  },
-  
-  
-};
+function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
+  return (
+    <Router>
+      <nav>
+        <Link to="/"> Home </Link>
+
+        {!isAuth ? (
+          <Link to="/login"> Login </Link>
+        ) : (
+          <>
+            <Link to="/createpost"> Create Post </Link>
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home isAuth={isAuth} />} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+      </Routes>
+    </Router>
+  );
+}
+
 export default App;
