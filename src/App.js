7aaa9e674a02c40d502fc0f7493292b71c2fd200
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-06 15:24:44 -0300

    add Folders:Create Post and LoginPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
+import Home from './pages/Home';
+import CreatPost from './pages/CreatePost';
+import Login from './pages/Login';
 function App() {
-  return 
-  <Router>
+  return <Router>
     <Routes>  
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/create" element={<CreatPost />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </Router>
 }
